9d7a50a4d72ccab28933965a26957a8a886fb365
zhenya <choshina@hotmail.com>
2022-04-05 09:22:28 +0800

    AT works

--- Online/examples/Autotrans_online/init_monitor.m
+++ Online/examples/Autotrans_online/init_monitor.m
@@ -6,11 +6,13 @@ rob_up_lim = -max_rob;  % stops when rob_up is below
 rob_low_lim = max_rob; 
 rob_diff_lim = -inf;
 
+diagnoser = 1;
+
 phi_autotrans = 'ev_[0,50] gear[t]>200';
 
 AT = BreachSimulinkSystem(mdl);
 
-AT.SetParam('Throttle_u0', 100);
+AT.SetParam({'Throttle_u0', 'diagnoser'}, [100, 2]);
 
 AT.Sim();
 AT.PlotSignals()
